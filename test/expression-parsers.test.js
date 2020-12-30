34629761d06aa96dffd4022ada6995b84a54268f
Ole van Santen <dxiedx@protonmail.com>
2020-12-30 18:19:39 +0100

    Adding additional tests for digit

--- test/expression-parsers.test.js
+++ test/expression-parsers.test.js
@@ -1,6 +1,7 @@
 import { assert } from 'chai';
 import { list, tuple, map, head, snd } from '../src/utils';
 import { digit, natural, fract, fixed } from '../src/expression-parsers';
+import {many} from "../src/parser-combinators";
 
 const firstParseResult= result => snd(head(result));
 
@@ -10,6 +11,18 @@ describe('Expression Parsers', () => {
     it('should recognize a digit characters and return the result as an integer', () => {
       const expected = list(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
       const actual = map(x => firstParseResult(digit(x)), list('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'));
+
+      assert.deepEqual(actual, expected);
+    });
+
+    it('should recognize a repeated digit characters if combined with many', () => {
+      const expected = list(
+        tuple('', list(1, 2, 3)),
+        tuple('3', list(1, 2)),
+        tuple('23', list(1)),
+        tuple('123', list()),
+      );
+      const actual = many(digit, '123');
 
       assert.deepEqual(actual, expected);
     });
